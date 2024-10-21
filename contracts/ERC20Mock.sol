8ff8cfc600fb1139c2d57ef73b3ac3e3c4bde2d4
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-10-21 13:17:59 -0700

    Mock ERC20 token

--- contracts/ERC20Mock.sol
+++ contracts/ERC20Mock.sol
@@ -13,7 +13,8 @@ contract ERC20Mock is ERC20 {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) external {
-        _burn(account, amount);
-    }
+    // function burn(address account, uint256 amount) external {
+    //     _burn(account, amount);
+    // }
+   
 }
