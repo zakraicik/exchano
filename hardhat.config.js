540d702f3504cb30c26e64093623693dca51afeb
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-10-25 08:30:11 -0700

    Forked Base network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.27'
+  solidity: '0.8.27',
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://base-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
+      }
+    }
+  }
 }
